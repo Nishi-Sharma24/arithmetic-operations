c7d845ccb1c6ee000aa300d11601498330ac4103
Nishi Sharma <84862478+nishisharma24@users.noreply.github.com>
2022-03-08 00:48:31 +0530

    Add files via upload

---
+++ arithmetic operation.py.py
@@ -0,0 +1,16 @@
+a=15
+b=10
+c=a+b
+d=a-b
+e=a*b
+f=a%b
+g=a/b
+h=a//b
+
+print(" The sum of a and b",c)
+print(" The subraction of a and b is", d)
+print(" The multiplication of a and b is", e)
+print(" The division of a and b is",g)
+print(" The modulus of a and b is",f)
+print(" the floor division of a and b is",h)
+input()
